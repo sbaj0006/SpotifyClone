081817734aee8c1484a85617a80f9f29a1ee38f4
Smarth Bajaj <sbaj0006@student.monash.edu>
2020-08-23 21:02:02 +1000

    Made changes to UI

--- src/Body.js
+++ src/Body.js
@@ -1,21 +1,35 @@
 import React from "react";
 import "./Body.css";
 import Header from "./Header";
+import { useDataLayerValue } from "./DataLayer";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import SongRow from "./SongRow";
 
 function Body({ spotify }) {
+  const [{ discover_weekly }, dispatch] = useDataLayerValue();
+
   return (
     <div className="body">
       <Header spotify={spotify} />
       <div className="body__info">
-        <img
-          src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
-          alt=""
-        />
+        <img src={discover_weekly?.images[0].url} alt="" />
         <div className="body__infoText">
           <strong>PLAYLIST</strong>
           <h2>Discover Weekly</h2>
-          <p>description...</p>
+          <p>{discover_weekly?.description}</p>
         </div>
+      </div>
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayCircleFilledIcon className="body__shuffle" />
+          <FavoriteIcon fontSize="large" />
+          <MoreHorizIcon />
+        </div>
+        {discover_weekly?.tracks.items.map((item) => (
+          <SongRow track={item.track} />
+        ))}
       </div>
     </div>
   );
--- src/reducer.js
+++ src/reducer.js
@@ -3,8 +3,8 @@ export const initialState = {
   playlists: [],
   playing: false,
   item: null,
-  token:
-    "BQDtQddjxMgAHl6NOZhEhuDfj6nJDcs-ABLtY8fS8nqgtg-OwGC_GxKW3Nqlqgf5gzuQ8f57S1jX75wkYuWkzMjO8EwVhyLf3OxHLoJaJahjFDHr8TUSzpn0TS08GteY4uQm2O0Z_9Vo5kRalvNmxO2eGX0WGHcz5ytgxI-1nXZ2U2ln",
+  // token:
+  //   "BQDtQddjxMgAHl6NOZhEhuDfj6nJDcs-ABLtY8fS8nqgtg-OwGC_GxKW3Nqlqgf5gzuQ8f57S1jX75wkYuWkzMjO8EwVhyLf3OxHLoJaJahjFDHr8TUSzpn0TS08GteY4uQm2O0Z_9Vo5kRalvNmxO2eGX0WGHcz5ytgxI-1nXZ2U2ln",
 };
 
 const reducer = (state, action) => {
@@ -24,6 +24,11 @@ const reducer = (state, action) => {
       return {
         ...state,
         playlists: action.playlists,
+      };
+    case "SET_DISCOVER_WEEKLY":
+      return {
+        ...state,
+        discover_weekly: action.discover_weekly,
       };
     default:
       return state;
